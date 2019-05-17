6995c81f43fafd1cbc71c5fbbe0e4153e7457dcc
Mackenzie Weaver <macknz7@gmail.com>
2019-05-17 20:24:30 -0300

    Update cash-register.js

--- cash-register.js
+++ cash-register.js
@@ -1,4 +1,4 @@
-// 5/6 tests to pass
+// 6/6  tests pass
 // https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/cash-register
 
 function checkCashRegister(price, cash, cid) {
@@ -26,12 +26,13 @@ function checkCashRegister(price, cash, cid) {
     return change;
   }
 
+  console.log(dif);
   // where the magic happens
   for (let i = 0; i < value.length; i++){ // is greater than 100? 20? 10? 5? 1? .25? .1? .05? .01?
     let arr = [];  
     let num = 0;
     if (dif >= value[i]){
-      num = value[i] * (Math.floor(dif/value[i]));
+      num = value[i] * (Math.floor(dif/value[i])); // .5 / .25 = 2 - 2 * .25 = .5
       if (num > cid[cid.length - 1 - i][1]){
         num = cid[cid.length - 1 - i][1];
       }
@@ -41,22 +42,25 @@ function checkCashRegister(price, cash, cid) {
       change.change.push(arr);
     }
   }
-  console.log(change.status);
-  console.log(change.change);
+  if(dif > 0){
+    change.status = 'INSUFFICIENT_FUNDS'
+    change.change = [];
+    return change;
+  }
   return change;
 }
 checkCashRegister(
-  3.26, 
-  100, 
+  19.5, 
+  20, 
   [
-    ["PENNY", 1.01],      // 0
-    ["NICKEL", 2.05],     // 1
-    ["DIME", 3.1],        // 2
-    ["QUARTER", 4.25],    // 3
-    ["ONE", 90],          // 4
-    ["FIVE", 55],         // 5
-    ["TEN", 20],          // 6
-    ["TWENTY", 60],       // 7
-    ["ONE HUNDRED", 100]  // 8
+    ["PENNY", 0.01],      // 0
+    ["NICKEL", 0],     // 1
+    ["DIME", 0],        // 2
+    ["QUARTER", 0],    // 3
+    ["ONE", 1],          // 4
+    ["FIVE", 0],         // 5
+    ["TEN", 0],          // 6
+    ["TWENTY", 0],       // 7
+    ["ONE HUNDRED", 0]  // 8
   ]
 );
