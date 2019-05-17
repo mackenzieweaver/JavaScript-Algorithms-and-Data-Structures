21c722f459e3a646fcd7246c674c604397fa5a9c
Mackenzie Weaver <macknz7@gmail.com>
2019-05-17 19:57:43 -0300

    Update cash-register.js

--- cash-register.js
+++ cash-register.js
@@ -1,10 +1,15 @@
 // 2/6 tests pass
 // https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/cash-register
+// test 3 - 'PENNY' off-by-one error (it's 3 when it should be 4)
 
 function checkCashRegister(price, cash, cid) {
+
   const name = ['ONE HUNDRED','TWENTY','TEN','FIVE','DOLLAR','QUARTER','DIME','NICKEL','PENNY'];
   const value = [100, 20, 10, 5, 1, .25, .1, .05, .01];
+  const ascVal = [.01, .05, .1, .25, 1, 5, 10, 20, 100];
+  const ascNam = ['PENNY', 'NICKEL', 'DIME', 'QUARTER', 'DOLLAR', 'FIVE', 'TEN', 'TWENTY', 'ONE HUNDRED'];
   let change = {'status': 'OPEN', 'change': []};
+
   // total in cash drawer
   let cidTotal = 0;
   for (let i = 0; i < cid.length; i++){
@@ -12,33 +17,38 @@ function checkCashRegister(price, cash, cid) {
   }
   let total = parseFloat(cidTotal.toFixed(2))
 
-  // example: 0.50 cents
+  // where the magic happens
   let dif = cash - price;
   for (let i = 0; i < value.length; i++){ // is greater than 100? 20? 10? 5? 1? .25? .1? .05? .01?
     let arr = [];  
     let num = 0;
     if (dif >= value[i]){
       num = value[i] * (Math.floor(dif/value[i]));
-      arr.push(name[i]);
+      if (num > cid[cid.length - 1 - i][1]){
+        num = cid[cid.length - 1 - i][1];
+      }
+      arr.push(name[i]);      
       arr.push(num);
       dif -= num;
       change.change.push(arr);
     }
   }
+  console.log(change.change);
   return change;
 }
 checkCashRegister(
-  19.50, 
-  20.00, 
+  3.26, 
+  100, 
   [
-    ["PENNY", 1.01], 
-    ["NICKEL", 2.05], 
-    ["DIME", 3.1], 
-    ["QUARTER", 4.25], 
-    ["ONE", 90], 
-    ["FIVE", 55], 
-    ["TEN", 20], 
-    ["TWENTY", 60], 
-    ["ONE HUNDRED", 100]
+    ["PENNY", 1.01],      // 0
+    ["NICKEL", 2.05],     // 1
+    ["DIME", 3.1],        // 2
+    ["QUARTER", 4.25],    // 3
+    ["ONE", 90],          // 4
+    ["FIVE", 55],         // 5
+    ["TEN", 20],          // 6
+    ["TWENTY", 60],       // 7
+    ["ONE HUNDRED", 100]  // 8
   ]
 );
+
