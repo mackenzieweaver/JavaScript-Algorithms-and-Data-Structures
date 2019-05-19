30e8c6fa91a94cf2f8cf80491edc980cd4b6840f
Mackenzie Weaver <macknz7@gmail.com>
2019-05-19 19:15:22 -0300

    Update spinal-case.js

--- spinal-case.js
+++ spinal-case.js
@@ -1,19 +1,20 @@
 function spinalCase(str) {
   let arr = str.match(/[A-Z]/g); // find the capitals
-  str = str.replace(/[_]|\s/g, '-').toLowerCase();
   str = str.split('');
   for (let i = 0; i < arr.length; i++){
     let index = str.indexOf(arr[i]);
-    if (index > 0){
-      str[index] = str[index].toLowerCase();
-      if(str[index - 1] !== ' '){
+    if (index > 0){ // not the first letter of the string
+      if(str[index - 1] !== ' ' &&  str[index - 1] !== '_'){
         str.splice(index, 0, ' ');
+      } else if (str[index - 1] === '_'){
+        str.splice(index - 1, 1, ' ');
       }
     }
   }
-
   str = str.join('');
+  str = str.replace(/[_]|\s/g, '-').toLowerCase();
   console.log(str);
   return str;
 }
-spinalCase('thisIsSpinalTap');
+
+spinalCase('The_Andy_Griffith_Show');
