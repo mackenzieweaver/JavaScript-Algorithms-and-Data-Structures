509bd8a274d89091f951021e2111c3e21868ed91
Mackenzie Weaver <macknz7@gmail.com>
2019-05-27 08:05:38 -0300

    Update sum-all-primes.js

--- sum-all-primes.js
+++ sum-all-primes.js
@@ -1,12 +1,23 @@
 // https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/sum-all-primes/
 function sumPrimes(num) {
-  let sum = 0;
+  let ans = 0;
   let arr = [];
-  for(let i = 2; i <= num; i++){
-    arr.push(i);
+
+  // Fill up the array
+  for (let k = 2; k <= num; k++){
+    arr.push(k);
+  }
+
+
+  for(let i = 2; i <= Math.floor(Math.sqrt(num)); i++){
+    for (let j = i*i; j <= num; j += i){
+      if(arr.includes(j)){
+        arr.splice(arr.indexOf(j), 1);
+      }
+    }
   }
   console.log(arr);
   return num;
 }
 
-sumPrimes(10);
+sumPrimes(30);
