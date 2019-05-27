e1693ad3b2b784d031f4a259b1d1144c773c1b61
Mackenzie Weaver <macknz7@gmail.com>
2019-05-27 08:09:42 -0300

    Update sum-all-primes.js

--- sum-all-primes.js
+++ sum-all-primes.js
@@ -8,14 +8,18 @@ function sumPrimes(num) {
     arr.push(k);
   }
 
-
+  // Loop the square root of the number amount of times (ex: sqrt(30) = 5)
   for(let i = 2; i <= Math.floor(Math.sqrt(num)); i++){
+    // Starting at the square (ex: 2 * 2  = 4) then add i (2, 3, 4, 5)
     for (let j = i*i; j <= num; j += i){
+      // If the number is still in the array
       if(arr.includes(j)){
+        // Remove that multiple of i (ex: 2, 4, 6, 8, 10)
         arr.splice(arr.indexOf(j), 1);
       }
     }
   }
+
   console.log(arr);
   return num;
 }
