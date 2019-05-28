229a11db50110fb046bc59967f545a153ce828f6
Mackenzie Weaver <macknz7@gmail.com>
2019-05-28 07:52:25 -0300

    Update smallest-common-multiple.js

--- smallest-common-multiple.js
+++ smallest-common-multiple.js
@@ -1,14 +1,21 @@
 //https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/smallest-common-multiple
 function smallestCommons(arr) {
   arr = (arr.sort((a, b) => a - b));
-  let start = arr[0];
-  let end = arr[1];
-  arr = [];
-  for(let i = start; i <= end; i++){
-    arr.push(i);
+  let newArr = [];
+  for(let i = arr[0]; i <= arr[1]; i++){
+    newArr.push(i);
   }
-  console.log(arr);
+  console.log(newArr);
+  let seguir = true;
+  let multiple = 2
+  let compare = 0;
+  while ( seguir ) {
+    compare = arr[1] * multiple;
+    newArr.filter(x => compare % x  === 0);
+    multiple += 1;
+  }
+  console.log(newArr);
 }
 
 
-smallestCommons([5, 1]);
+smallestCommons([6, 1]);
