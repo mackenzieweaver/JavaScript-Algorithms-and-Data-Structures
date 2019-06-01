298d29aaf3ecec00b8888cae152afa9f93f8abe5
Mackenzie Weaver <macknz7@gmail.com>
2019-06-01 07:54:57 -0300

    Update arguments.optional.js

--- arguments.optional.js
+++ arguments.optional.js
@@ -1,7 +1,13 @@
 // https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/arguments-optional
 function addTogether(...args) {
   if (args.every(x => typeof x === 'number')){
-    
+    if(args[0] && !args[1]){
+      return function(arg) {
+        return args[0] + arg;
+      }
+    } else {
+      return args[0] + args[1];
+    }
   }
   return;
 }
