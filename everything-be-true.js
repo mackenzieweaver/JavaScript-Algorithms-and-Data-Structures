031fb87fbf5d5d6b368b171aedf2627ffcb9d666
Mackenzie Weaver <macknz7@gmail.com>
2019-06-01 07:37:29 -0300

    Update everything-be-true.js

--- everything-be-true.js
+++ everything-be-true.js
@@ -1,8 +1,15 @@
 //https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/everything-be-true
 function truthCheck(collection, pre) {
-  collection.forEach(x => {if(!x.hasOwnProperty(pre)){return false}});
+  for(let i = 0; i < collection.length; i++){
+    if(!collection[i][pre]){
+      return false;
+    }
+  }
   return true;
 }
-//false, 0, "", null, undefined and NaN).
 
-truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
+truthCheck([{"user": "Tinky-Winky", "sex": "male", "age": 0}, 
+            {"user": "Dipsy", "sex": "male", "age": 3}, 
+            {"user": "Laa-Laa", "sex": "female", "age": 5}, 
+            {"user": "Po", "sex": "female", "age": 4}], 
+           "age");
