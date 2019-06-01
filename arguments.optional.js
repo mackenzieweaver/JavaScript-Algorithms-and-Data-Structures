be1a0cba781bcfa40724191dcddbc688f791ae79
Mackenzie Weaver <macknz7@gmail.com>
2019-06-01 07:50:23 -0300

    Create arguments.optional.js

---
+++ arguments.optional.js
@@ -0,0 +1,9 @@
+// https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/arguments-optional
+function addTogether(...args) {
+  if (args.every(x => typeof x === 'number')){
+    
+  }
+  return;
+}
+
+addTogether(2,3);
