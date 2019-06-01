b76bc9412f8926a8836aea65962b893f2831a241
Mackenzie Weaver <macknz7@gmail.com>
2019-06-01 07:58:08 -0300

    Update arguments.optional.js

--- arguments.optional.js
+++ arguments.optional.js
@@ -1,11 +1,18 @@
 // https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/arguments-optional
 function addTogether(...args) {
+  // make sure all arguments are numbers
   if (args.every(x => typeof x === 'number')){
+    // if we have the first number but not the second
     if(args[0] && !args[1]){
+      // return a function
       return function(arg) {
+        // which adds the number that was given plus another
         return args[0] + arg;
       }
-    } else {
+    } 
+    // otherwise, if we have both numbers
+    else {
+      // return their sum
       return args[0] + args[1];
     }
   }
