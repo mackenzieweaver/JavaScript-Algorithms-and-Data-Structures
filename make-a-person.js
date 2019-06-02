98c9a01bce4d12227f85846936976fb54d0e23c1
Mackenzie Weaver <macknz7@gmail.com>
2019-06-02 09:22:23 -0300

    Update make-a-person.js

--- make-a-person.js
+++ make-a-person.js
@@ -1,27 +1,27 @@
 // https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/make-a-person/
-var Person = function(firstAndLast) {
-  let names = {
-    'first': firstAndLast.split(' ')[0],
-    'last': firstAndLast.split(' ')[1],
+var Person = function(firstAndLast) { // class definition
+  let names = { // names object to store first and last
+    'first': firstAndLast.split(' ')[0], // initially the first name depends on the input when the object is created
+    'last': firstAndLast.split(' ')[1], // initially the last name depends on the input when the object is created
   }
-  this.getFullName = function(){ 
+  this.getFullName = function(){ // check names object and concatenate first and last
     return names.first + ' ' + names.last;
   };
-  this.getFirstName = function(){
+  this.getFirstName = function(){ // fetch first from names object
     return names.first;
   };
-  this.getLastName = function() {
+  this.getLastName = function() { // fetch last from names object
     return names.last;
   };
-  this.setFullName = function(input) {
-    names.first = input.split(' ')[0];
-    names.last = input.split(' ')[1];
+  this.setFullName = function(input) { // split the input into first and last
+    names.first = input.split(' ')[0]; // set the names object first property
+    names.last = input.split(' ')[1]; // set the names object last property
   }
-  this.setFirstName = function(input) {
+  this.setFirstName = function(input) { // set names object first property to the input
     names.first = input;
   }
-  this.setLastName = function(input) {
+  this.setLastName = function(input) { // set names object last property to the input
     names.last = input;
   }
 };
-var bob = new Person('Bob Ross');
+var bob = new Person('Bob Ross'); // create an instance of the Person class
