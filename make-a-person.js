e1c715e60fb018ec533bf8318b093039848f51c8
Mackenzie Weaver <macknz7@gmail.com>
2019-06-02 09:19:27 -0300

    Update make-a-person.js

--- make-a-person.js
+++ make-a-person.js
@@ -1,27 +1,27 @@
 // https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/make-a-person/
 var Person = function(firstAndLast) {
-  this.names = {
+  let names = {
     'first': firstAndLast.split(' ')[0],
     'last': firstAndLast.split(' ')[1],
   }
   this.getFullName = function(){ 
-    return this.names.first + ' ' + this.names.last;
+    return names.first + ' ' + names.last;
   };
   this.getFirstName = function(){
-    return this.names.first;
+    return names.first;
   };
   this.getLastName = function() {
-    return this.names.last;
+    return names.last;
   };
   this.setFullName = function(input) {
-    this.names.first = input.split(' ')[0];
-    this.names.last = input.split(' ')[1];
+    names.first = input.split(' ')[0];
+    names.last = input.split(' ')[1];
   }
   this.setFirstName = function(input) {
-    this.names.first = input;
+    names.first = input;
   }
   this.setLastName = function(input) {
-    this.names.last = input;
+    names.last = input;
   }
 };
 var bob = new Person('Bob Ross');
